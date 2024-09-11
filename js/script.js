85032ad156864da01762ad7478ff082f398dff80
Kenza <a.kenza29@gmail.com>
2024-09-11 16:55:00 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const inputCP = document.querySelector(".cp")
+const selectVille = document.querySelector(".ville")
+
+inputCP.addEventListener("input", ()=>{
+    let value =inputCP.value
+
+    
+})
+
